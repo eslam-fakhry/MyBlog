3a481b80fa290daccc35ef6f1aa3a06fc965a549
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-11-24 01:25:19 +0200

    Create PaginatedCollection.cs

---
+++ MyBlog/Utilities/PaginatedCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Utilities
+{
+    public class PaginatedCollection<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedCollection(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => (PageIndex > 1);
+
+        public bool HasNextPage => (PageIndex < TotalPages);
+    }
+}
