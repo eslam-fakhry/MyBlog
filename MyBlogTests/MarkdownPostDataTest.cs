7bdfffdfe875a90bf96ef999ccb9f242549a764c
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-11-24 01:32:31 +0200

    Rename mock object name

--- MyBlogTests/MarkdownPostDataTest.cs
+++ MyBlogTests/MarkdownPostDataTest.cs
@@ -2,8 +2,6 @@
 using MyBlog.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
 using Xunit;
 
 namespace MyBlogTests
@@ -13,8 +11,8 @@ namespace MyBlogTests
         [Fact]
         public void Get_ReturnsTheRightPost()
         {
-            string slug = "my-first-blog-post";
-            string markdown = @"---
+            var slug = "my-first-blog-post";
+            var markdown = @"---
 title:Game Development with C#
 tags: c# .net game
 date:1-may-2019
@@ -22,18 +20,17 @@ excerpt:I will tell you all about that game development
 ---
 # heading 1";
 
-            var fileMarkdownGetter = new Mock<IMarkdownGetter>();
-            fileMarkdownGetter.Setup(x => x.GetBySlug(It.IsAny<string>())).Returns(() => markdown);
-            var markdownPostData = new MarkdownPostData(fileMarkdownGetter.Object);
+            var markdownGetter = new Mock<IMarkdownGetter>();
+            markdownGetter.Setup(x => x.GetBySlug(It.IsAny<string>())).Returns(() => markdown);
+            var markdownPostData = new MarkdownPostData(markdownGetter.Object);
 
             var post = markdownPostData.GetBySlug(slug);
 
             Assert.Equal("<h1>heading 1</h1>", post.Content.Trim());
-            Assert.Equal(new List<string> { "c#", ".net", "game" }, post.Tags);
+            Assert.Equal(new List<string> {"c#", ".net", "game"}, post.Tags);
             Assert.Equal(DateTime.Parse("1-may-2019"), post.PublishDate);
             Assert.Equal("I will tell you all about that game development", post.Excerpt);
             Assert.Equal("Game Development with C#", post.Title);
-
         }
 
     }
