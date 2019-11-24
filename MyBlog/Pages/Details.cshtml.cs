3dff629e4f078c70f5cbb31f979b718029e41323
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-11-24 16:11:11 +0200

    Add Details razor page

---
+++ MyBlog/Pages/Details.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBlog.Pages
+{
+    public class Details : PageModel
+    {
+        public void OnGet(string slug)
+        {
+            
+        }
+    }
+}
