882b096d8c1b46f667cfcb09ac36564f7d98d471
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-12-13 04:08:59 +0200

    Add seeder for admin user credentials

---
+++ MyBlog/Models/SeedAdminCredentials.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlog.Areas.Identity.Data;
+
+namespace MyBlog.Models
+{
+    public static class SeedAdminCredentials
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var config = serviceProvider.GetRequiredService<IConfiguration>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<MyBlogUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var x = await roleManager.RoleExistsAsync("Admin");
+            if (!x)
+            {
+                var role = new IdentityRole {Name = "Admin"};
+                await roleManager.CreateAsync(role);
+
+                var user = new MyBlogUser {UserName = "admin", Email = config["AdminEmail"]};
+                var userPwd = config["AdminPassword"];
+                var createdUser = await userManager.CreateAsync(user, userPwd);
+
+                if (createdUser.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
+    }
+}
